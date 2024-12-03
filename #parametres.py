41eb40dc3a3cd8ac5eb14851a2178bcb8ef344c9
Ivanleon123 <imunyoz20974@iesjoanramis.org>
2024-12-03 08:40:52 +0100

    Add files via upload

---
+++ #parametres.py
@@ -0,0 +1,11 @@
+#parametres
+#string/cadena de caracters ?? es passa per valor o per referencia
+def axllista(a, b, c):
+    s="aixo es un canvi, anam a veure si fora de la funcio queda el canvi o no"
+    c= a + b
+a = 3
+b = 4
+c = 0
+print(c)
+operacio(a, b, c)
+print(c)
